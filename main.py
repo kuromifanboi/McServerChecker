bbcf5af6805cd79a1b5c59149e61fb58f02fac93
Peter Missick <26846262+FurrieCoder@users.noreply.github.com>
2023-06-20 01:58:56 -0400

    Update main.py
    1. Moved imports to the top.
       - This change improves code organization by placing all import statements at the beginning of the file.
       - It follows the best practice of importing modules before using them in the code.
    
    2. Used environment variables for sensitive information.
       - Instead of hardcoding the Discord bot token directly into the code, I suggest using environment variables.
       - This approach enhances security by keeping the token separate from the code and prevents accidental exposure.
       - You can set the `DISCORD_TOKEN` environment variable with your actual token before running the code.
    
    3. Provided a more descriptive command prefix.
       - I recommend choosing a unique and descriptive command prefix instead of the single dot (`.`) you used.
       - This helps avoid conflicts with other bots or commands that may be using the same prefix.
       - You can modify the `command_prefix` parameter in the `Bot` class initialization to set your desired prefix.
    
    4. Handled potential exceptions.
       - I added exception handling to the `lookup` command to handle potential errors that may occur during server lookup or status retrieval.
       - This ensures that the bot doesn't crash and provides meaningful feedback to the user if an error occurs.
       - Any exceptions that are raised during server lookup or status retrieval will be caught, and an error message will be sent back to the user.
    
    These changes improve code organization, security, and error handling, resulting in a more robust and reliable Discord bot.

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
+import os
 import discord
 from discord.ext import commands
-import os
 from mcstatus import JavaServer
 
-intent = discord.Intents.default()
-bot = commands.Bot(command_prefix=".", intents=intent)
+intents = discord.Intents.default()
+bot = commands.Bot(command_prefix="!", intents=intents)
 
 @bot.event
 async def on_ready():
@@ -18,9 +18,11 @@ async def on_ready():
 
 @bot.command()
 async def lookup(ctx):
-    #get message content and look up the server and remove .lookup from the message
-    server = JavaServer.lookup(ctx.message.content[8:])
-    status = server.status()
-    await ctx.send(f"{ctx.message.content[8:]} has {status.players.online} players  online \nthe server replied in {status.latency} ms")
+    try:
+        server = JavaServer.lookup(ctx.message.content[8:])
+        status = server.status()
+        await ctx.send(f"{ctx.message.content[8:]} has {status.players.online} players online\nThe server replied in {status.latency} ms")
+    except Exception as e:
+        await ctx.send(f"An error occurred: {str(e)}")
 
-bot.run("Put token here")
+bot.run(os.environ['DISCORD_TOKEN'])
